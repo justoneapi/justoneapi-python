1b8a1daa5f62a5fb43b5e90a33cca7cacc8f6d37
Anonymous Committer <anonymous@users.noreply.github.com>
2025-08-24 00:27:47 +0800

    release: update comment

--- justoneapi/log.py
+++ justoneapi/log.py
@@ -2,7 +2,6 @@ import logging
 
 logger = logging.getLogger("justoneapi")
 
-# 如果用户未配置过 handler，则添加默认控制台输出
 if not logger.hasHandlers():
     handler = logging.StreamHandler()
     formatter = logging.Formatter(
